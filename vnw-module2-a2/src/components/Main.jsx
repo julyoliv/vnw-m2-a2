1aeba6008883ffabae3784ef9f5faeda00acdc1e
julyoliv <tech.julyoliv@gmail.com>
2024-02-28 12:16:14 -0300

    vnw module 2, a2

---
+++ vnw-module2-a2/src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Main() {
+    return (
+        <main>
+            <h1>"If I had the offer to do it again,
+                 make me invincible, invisible, or brain dead.
+                 If the body and the life were two things that we could divide
+                 I'd deliver up my shell to be filled with somebody else."</h1>
+            <h2>The Shell, by Lucy Dacus</h2>
+            <h2>Historian (2018)</h2>
+            <iframe width="600" height="450git" src="https://www.youtube.com/embed/qZO7wSUeNOk" title="The Shell" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </main>
+    )
+}
+
+export default Main
